c3b61df92664ad94c95814707cda7c6b920340c6
nakato156 <christianvelasces@gmail.com>
2023-09-07 17:51:42 -0500

    fix: arreglo grafo

--- classes/graph.py
+++ classes/graph.py
@@ -1,14 +1,17 @@
-class Nodo:
+class Vertice:
     def __init__(self, valor) -> None:
         self.valor = valor
     
+    def __hash__(self) -> int:
+        return hash(self.valor)
+
     def __repr__(self) -> str:
         return f"{self.valor}"
 
 class Arista:
-    def __init__(self, vertice1: Nodo, vertice2: Nodo, peso: int) -> None:
-        self.vertice1: Nodo = vertice1
-        self.vertice2: Nodo = vertice2
+    def __init__(self, vertice1: Vertice, vertice2: Vertice, peso: int) -> None:
+        self.vertice1: Vertice = vertice1
+        self.vertice2: Vertice = vertice2
         self.peso: int = peso
     
     def __iter__(self):
@@ -22,13 +25,14 @@ class Arista:
     
 class Grafo:
     def __init__(self, vertices:dict) -> None:
-        self.vertices: dict[Nodo, list[tuple[Nodo, int]]] = vertices or {}
+        self.vertices: dict[Vertice, list[tuple[Vertice, int]]] = vertices or {}
 
-    def agregar_vertice(self, vertice):
+    def agregar_vertice(self, vertice: Vertice | str):
+        vertice: Vertice = Vertice(vertice) if type(vertice) == str else vertice 
         if vertice not in self.vertices:
             self.vertices[vertice] = []
     
-    def obtener_vertices(self) -> list[Nodo]:
+    def obtener_vertices(self) -> list[Vertice]:
         return self.vertices.keys()
 
     def obtener_aristas(self) -> list[Arista]:
